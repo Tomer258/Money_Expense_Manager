a70d2e859ae46a8bddc745c3158003999544b5dd
Tomer258 <tomer25898@gmail.com>
2023-05-23 19:21:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/moneyexpensemanager/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.moneyexpensemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.moneyexpensemanager.databinding.ActivityMainBinding;
+import com.example.moneyexpensemanager.fragments.Charts_fragment;
+import com.example.moneyexpensemanager.fragments.Personal_fragment;
+import com.example.moneyexpensemanager.fragments.Settings_fragment;
+import com.example.moneyexpensemanager.fragments.Shared_expense_fragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.bottomNavigationView.setBackground(null);
+        replaceFragment(new Personal_fragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int c_id=item.getItemId();
+            if (c_id==R.id.bottom_menu_transaction)
+                replaceFragment(new Personal_fragment());
+            if (c_id==R.id.bottom_menu_Shared)
+                replaceFragment(new Shared_expense_fragment());
+            if (c_id==R.id.bottom_menu_Stats)
+                replaceFragment(new Charts_fragment());
+            if (c_id==R.id.bottom_menu_more)
+                replaceFragment(new Settings_fragment());
+
+            return true;
+        });
+
+    }
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout_container, fragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/moneyexpensemanager/fragments/Shared_expense_fragment.java
@@ -0,0 +1,21 @@
+package com.example.moneyexpensemanager.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.moneyexpensemanager.R;
+
+public class Shared_expense_fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_shared_expense_fragment, container, false);
+    }
+}
